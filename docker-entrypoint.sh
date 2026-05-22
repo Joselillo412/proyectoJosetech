f1fee94963d23875a7811a0ba9c8492f44427843
Jose Buendia <josebuendiavico4@gmail.com>
2026-05-22 21:14:07 +0200

    fix: corregir orden en entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,10 +3,12 @@ set -e
 
 echo "Limpiando caché de configuración..."
 php artisan config:clear
-php artisan cache:clear
 
 echo "Ejecutando migraciones..."
 php artisan migrate --force
+
+echo "Limpiando caché de aplicación..."
+php artisan cache:clear
 
 echo "Iniciando Apache..."
 apache2-foreground
