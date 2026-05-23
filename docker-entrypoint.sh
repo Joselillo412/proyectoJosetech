2256a520ebecffc8edca9f935a49b3931345e878
Jose Buendia <josebuendiavico4@gmail.com>
2026-05-23 19:13:19 +0200

    fix: añadir .htaccess de Laravel2

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -e
 
-echo "Limpiando caché de configuración..."
+echo "Limpiando caché..."
 php artisan config:clear
 
 echo "Ejecutando migraciones..."
 php artisan migrate --force
+
+echo "Ejecutando seeders..."
+php artisan db:seed --force
 
 echo "Iniciando Apache..."
 apache2-foreground
