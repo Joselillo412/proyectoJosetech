6ccefbd6a19b07c4dc1d196a03861817798d79e8
Jose Buendia <josebuendiavico4@gmail.com>
2026-05-23 18:25:47 +0200

    fix: quitar cache:clear

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,8 +7,5 @@ php artisan config:clear
 echo "Ejecutando migraciones..."
 php artisan migrate --force
 
-echo "Limpiando caché de aplicación..."
-php artisan cache:clear
-
 echo "Iniciando Apache..."
 apache2-foreground
