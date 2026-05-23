3d857ec8d6cc3ca4e5d47b7be9f50f8c04161f6a
Jose Buendia <josebuendiavico4@gmail.com>
2026-05-23 19:50:14 +0200

    solucionar migraciones

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,8 +7,5 @@ php artisan config:clear
 echo "Ejecutando migraciones..."
 php artisan migrate --force
 
-echo "Ejecutando seeders..."
-php artisan db:seed --force
-
 echo "Iniciando Apache..."
 apache2-foreground
